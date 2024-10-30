48b3cba86e69eecc770dc92eede3236f1fe579d1
AleNavarro <ale.naengram@gmail.com>
2024-10-30 13:32:48 +0100

    Arreglados conflictos

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ function App() {
   return (
     <div className="App">
 
-    <div className='contenedor-principal'>
-      <div className='contenedor-pregunta'>
-
-      </div>
-      <div className='contenedor-respuesta'>
-
-      </div>
-      <div className='contenedor-juego'>
-        <div className='contenedor-botones'>
+      <div className='contenedor-principal'>
+        <div className='contenedor-pregunta'>
 
         </div>
-        <div className='contenedor-animacion'>
+        <div className='contenedor-respuesta'>
 
         </div>
+        <div className='contenedor-juego'>
+          <div className='contenedor-botones'>
+
+          </div>
+          <div className='contenedor-animacion'>
+
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
