f5dcc9d6a68a0688193c1c7c0d3cb5b7c6ea9d06
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-10-30 12:06:00 +0100

    Creada estructura de la página

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+
+    <div className='contenedor-principal'>
+      <div className='contenedor-pregunta'>
+
+      </div>
+      <div className='contenedor-respuesta'>
+
+      </div>
+      <div className='contenedor-juego'>
+        <div className='contenedor-botones'>
+
+        </div>
+        <div className='contenedor-animacion'>
+
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
