4e7bdb0776ea9119669d500ab8d1998043fef87e
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-10-30 11:03:19 +0100

    Creado componente de prueba

---
+++ src/components/Prueba.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Prueba(props) {
+  return (
+    <div>Prueba</div>
+  )
+}
+
+Prueba.propTypes = {}
+
+export default Prueba
