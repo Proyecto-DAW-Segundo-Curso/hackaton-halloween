e14e4d278d4176de6b0759ebb1fd91c1937cab13
AleNavarro <ale.naengram@gmail.com>
2024-10-30 13:23:16 +0100

    comonente boton

---
+++ src/components/Boton.jsx
@@ -0,0 +1,18 @@
+//import React from 'react'
+import '../styles/Boton.css';
+
+function Boton(props) {
+	const esInicio = (valor) => {
+		cajaBoton = () => {
+			
+		}
+	}
+
+	return (
+		<div className="cajaBoton"></div>
+
+
+	);
+}
+
+export default Boton;
